19239575dc72b220faed1f4a7e21144b3c78ca47
Riyaonweb <riyahyf@gmail.com>
2024-02-06 23:36:27 +0530

    Add files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const choices = ["rock","paper","scissor"];
-
 let player;
 let computer;
+
 
 //computer choice
 function computerChoice(){
@@ -12,11 +12,18 @@ function computerChoice(){
 
 //player choice
 function playerChoice(){
-    player = prompt("give your choice");
-    console.log(player);
-    return player;
+const buttons = document.querySelectorAll('button');
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        player = e.target.textContent;
+       console.log(player);
+    })
+      
+   
+})
+return player;
 }
-
+playerChoice();
 //Round
 function playRound(computerSel,playerSel){
     return (playerSel == computerSel) ? "Its a Tie" :
@@ -26,37 +33,37 @@ function playRound(computerSel,playerSel){
 
    
 }
-
+playRound(player,computer);
 // 5 Round
 
 
-function playGame(){
-    let playerScore = 0;
-    let computerScore = 0;
+// function playGame(){
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    for(var i=0; i<5; i++){
-        let playerSelection = playerChoice();
-        let computerSelection = computerChoice();
-      let result = playRound(computerSelection,playerSelection);
-        console.log(result);
-        if(result == "computer won"){
-            computerScore++;
-            console.log("computer Score",computerScore);
-        }else if(result == "player won"){
-            playerScore++;
-            console.log("playerscore",playerScore);
-        }
-    }
-    //result announcement
-    if(computerScore > playerScore){
-        console.log("The Winner is Computer");
-    }else if (computerScore < playerScore){
-        console.log("The Winner is Player");
-    }else{
-        console.log("Its a tie")
-    }
-}
-playGame();
+//     for(var i=0; i<5; i++){
+//         let playerSelection = playerChoice();
+//         let computerSelection = computerChoice();
+//       let result = playRound(computerSelection,playerSelection);
+//         console.log(result);
+//         if(result == "computer won"){
+//             computerScore++;
+//             console.log("computer Score",computerScore);
+//         }else if(result == "player won"){
+//             playerScore++;
+//             console.log("playerscore",playerScore);
+//         }
+//     }
+//     //result announcement
+//     if(computerScore > playerScore){
+//         console.log("The Winner is Computer");
+//     }else if (computerScore < playerScore){
+//         console.log("The Winner is Player");
+//     }else{
+//         console.log("Its a tie")
+//     }
+// }
+// playGame();
 
 
 
